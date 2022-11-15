99a823c729827be00bf033da69ddc05f92dca49b
noobmasterweeb <farhad.rahimi.mylove77@gmail.com>
2022-11-15 05:37:40 -0800

    make a rest button

---
+++ lib/ResetButton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ResetButton extends StatelessWidget {
+  Function reset;
+  String text;
+
+  ResetButton(this.reset, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style:ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.purple),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder( borderRadius: new BorderRadius.circular(8.0))),
+        fixedSize: MaterialStateProperty.all(Size(100, 50))
+      ),
+      onPressed: reset,
+
+      child: Text("Reset",
+        style:TextStyle(
+          fontSize: 25
+        ) ,),
+
+    );
+  }
+}
