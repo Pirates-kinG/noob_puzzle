d1836c7c5bb573740a6f0fef83cfaf9c76964f2b
unknown <sinanazari110@gmail.com>
2022-11-16 01:32:54 +0430

    Board structure created

---
+++ lib/Board.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'Menu.dart';
+import 'MyTitle.dart';
+import 'Grid.dart';
+
+class Board extends StatefulWidget {
+  @override
+  _BoardState createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  int move = 0;
+
+  static const duration = const Duration(seconds: 1);
+  int secondsPassed = 0;
+  bool isActive = false;
+  Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    numbers.shuffle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    if (timer == null) {
+      timer = Timer.periodic(duration, (Timer t) {
+        startTime();
+      });
+    }
+
+    return SafeArea(
+      child: Container(
+        height: size.height,
+        color: Colors.blue,
+        child: Column(
+          children: <Widget>[
+            MyTitle(size),
+            Grid(numbers, size, clickGrid),
+            Menu(reset, move, secondsPassed, size),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void clickGrid(index) {
+    if (secondsPassed == 0) {
+      isActive = true;
+    }
+    if (index - 1 >= 0 && numbers[index - 1] == 0 && index % 4 != 0 ||
+        index + 1 < 16 && numbers[index + 1] == 0 && (index + 1) % 4 != 0 ||
+        (index - 4 >= 0 && numbers[index - 4] == 0) ||
+        (index + 4 < 16 && numbers[index + 4] == 0)) {
+      setState(() {
+        move++;
+        numbers[numbers.indexOf(0)] = numbers[index];
+        numbers[index] = 0;
+      });
+    }
+    checkWin();
+  }
+
+
+
+
+}
