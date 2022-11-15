ce6615ab389672fa7dbed26a1318912050ec7049
noobmasterweeb <farhad.rahimi.mylove77@gmail.com>
2022-11-15 05:31:44 -0800

    make grid button for the project

---
+++ lib/GridButton.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GridButton extends StatelessWidget {
+  Function click;
+  String text;
+
+  GridButton(this.text, this.click);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style:ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.purple),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder( borderRadius: new BorderRadius.circular(8.0)))
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      onPressed: click,
+    );
+  }
+}
